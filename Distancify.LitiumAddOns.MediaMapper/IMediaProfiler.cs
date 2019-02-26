07aacfcae172a9253eac8d3d05f38236384ccabb
Kristoffer Lindvall <kristoffer@distancify.com>
2019-02-26 21:20:59 +0100

    Add some documentation for IMediaProfiler.GetMediaProfile

--- Distancify.LitiumAddOns.MediaMapper/IMediaProfiler.cs
+++ Distancify.LitiumAddOns.MediaMapper/IMediaProfiler.cs
@@ -8,6 +8,11 @@ namespace Distancify.LitiumAddOns.MediaMapper
         Lifetime = DependencyLifetime.Singleton)]
     public interface IMediaProfiler
     {
+        /// <summary>
+        /// Creates a MediaProfile for a media item
+        /// </summary>
+        /// <param name="builder">A MediaProfileBuilder instance of the current item</param>
+        /// <returns>If null, MediaMapper will leave the media item alone to stay in the upload folder</returns>
         MediaProfile GetMediaProfile(MediaProfileBuilder builder);
     }
 }
