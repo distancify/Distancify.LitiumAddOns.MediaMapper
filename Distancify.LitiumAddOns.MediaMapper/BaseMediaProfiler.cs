aee443a9d13cc64e7fc9797a68c9c4bb0c563958
Vlad Szabo <vlad@distancify.com>
2019-07-23 12:54:02 +0300

    Update to 7.2.3

---
+++ Distancify.LitiumAddOns.MediaMapper/BaseMediaProfiler.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Litium.Media;
+
+namespace Distancify.LitiumAddOns.MediaMapper
+{
+    public abstract class BaseMediaProfiler : IMediaProfiler
+    {
+        protected abstract MediaProfile CreateMediaProfile(File file);
+
+        public MediaProfile GetMediaProfile(MediaProfileBuilder builder)
+        {
+            var profile = CreateMediaProfile(builder.File);
+
+            if (profile == null || !profile.MediaEntityMappings.Any()) return null;
+
+            var productId = profile.MediaEntityMappings.Select(r => r.EntityId).OrderBy(r => r).First();
+            var archivePath = profile.ArchivePath ?? string.Format("{0}/{1}", productId.Substring(productId.Length - 2), productId);
+
+            foreach (var entity in profile.MediaEntityMappings)
+            {
+                builder.MapTo(entity.EntityType, entity.EntityId, entity.FieldId);
+            }
+
+            foreach (var field in profile.Fields)
+            {
+                builder.SetField(field.Key, field.Value);
+            }
+
+            return builder.Create();
+        }
+
+        public abstract bool HasMatchingProfile(string fileName);
+    }
+}
