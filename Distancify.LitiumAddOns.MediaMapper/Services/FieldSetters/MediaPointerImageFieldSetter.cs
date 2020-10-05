958771f28447f88da09ffbcf92fb6beaac1ed665
Paul Birsan <paul@distancify.com>
2020-10-05 14:23:41 +0300

    Implement the media pointer image field setter; Add support for multi field mapping

---
+++ Distancify.LitiumAddOns.MediaMapper/Services/FieldSetters/MediaPointerImageFieldSetter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Litium.FieldFramework;
+using Litium.Media;
+using Litium.Runtime.DependencyInjection;
+
+namespace Distancify.LitiumAddOns.MediaMapper.Services.FieldSetters
+{
+    [Service(ServiceType = typeof(IFieldSetter))]
+    public class MediaPointerImageFieldSetter : IFieldSetter
+    {
+        public bool CanSet(IFieldDefinition field)
+        {
+            return field.FieldType.Equals(SystemFieldTypeConstants.MediaPointerImage, StringComparison.Ordinal);
+        }
+
+        public void Set(FieldContainer entity, IFieldDefinition field, File file)
+        {
+            entity.AddOrUpdateValue(field.Id, file.SystemId);
+        }
+    }
+}
