43f9f7d0f5d09adc77ab13346c261bb3883aa148
Rikard Åkesson <rikard@distancify.com>
2020-10-20 19:06:56 -0700

    Remove code hogging lock

--- Distancify.LitiumAddOns.MediaMapper/Runner.cs
+++ Distancify.LitiumAddOns.MediaMapper/Runner.cs
@@ -4,9 +4,7 @@ using Litium.Owin.InversionOfControl;
 using Litium.Events;
 using Litium.Runtime;
 using Litium.Media.Events;
-using System.Threading;
 using Litium.Foundation;
-using System.Threading.Tasks;
 using System.Configuration;
 using Distancify.SerilogExtensions;
 using Litium.Runtime.DistributedLock;
@@ -21,18 +19,14 @@ namespace Distancify.LitiumAddOns.MediaMapper
     [Autostart]
     public class Runner
     {
-        private Task _task;
-        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         private const string lockKey = "DistancifyMediaMapperRunner";
 
         public Runner(
             IIoCContainer container,
             EventBroker eventBroker,
-            IApplicationLifetime applicationLifetime,
             DistributedLockService distributedLockService) : this(
             container,
             eventBroker,
-            applicationLifetime,
             distributedLockService,
             ConfigurationManager.AppSettings["MediaMapperEnabled"])
         {
@@ -42,7 +36,6 @@ namespace Distancify.LitiumAddOns.MediaMapper
         public Runner(
             IIoCContainer container,
             EventBroker eventBroker,
-            IApplicationLifetime applicationLifetime,
             DistributedLockService distributedLockService,
             string enabledSetting)
         {
@@ -63,42 +56,6 @@ namespace Distancify.LitiumAddOns.MediaMapper
                     }
                 }
             });
-
-            applicationLifetime.ApplicationStarted.Register(() =>
-            {
-                _task = Task.Run(() =>
-                {
-                    using (Solution.Instance.SystemToken.Use())
-                    using (distributedLockService.AcquireLock(lockKey, TimeSpan.FromSeconds(10)))
-                    {
-                        while (!_cancellationTokenSource.Token.IsCancellationRequested)
-                        {
-                            if (SleepUnlessCancelled(5, _cancellationTokenSource.Token)) return;
-
-                            foreach (var m in container.ResolveAll<IMediaMapper>())
-                            {
-                                m.Map();
-                            }
-                        }
-                    }
-                }, _cancellationTokenSource.Token);
-            });
-
-            applicationLifetime.ApplicationStopping.Register(() =>
-            {
-                _cancellationTokenSource.Cancel();
-                subscription.Dispose();
-            });
-        }
-
-        private bool SleepUnlessCancelled(int minutes, CancellationToken token)
-        {
-            for (int i = 0; i < minutes * 60; i++)
-            {
-                if (_cancellationTokenSource.Token.IsCancellationRequested) return _cancellationTokenSource.Token.IsCancellationRequested;
-                Thread.Sleep(1000);
-            }
-            return _cancellationTokenSource.Token.IsCancellationRequested;
         }
     }
 }
