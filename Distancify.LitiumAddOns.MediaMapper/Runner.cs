1510087aef54ac85a14b93698e51e0546cc783e1
Rikard Åkesson <rikard@distancify.com>
2020-11-24 22:25:11 -0800

    Change location of lock in runner

--- Distancify.LitiumAddOns.MediaMapper/Runner.cs
+++ Distancify.LitiumAddOns.MediaMapper/Runner.cs
@@ -4,7 +4,9 @@ using Litium.Owin.InversionOfControl;
 using Litium.Events;
 using Litium.Runtime;
 using Litium.Media.Events;
+using System.Threading;
 using Litium.Foundation;
+using System.Threading.Tasks;
 using System.Configuration;
 using Distancify.SerilogExtensions;
 using Litium.Runtime.DistributedLock;
@@ -19,14 +21,18 @@ namespace Distancify.LitiumAddOns.MediaMapper
     [Autostart]
     public class Runner
     {
+        private Task _task;
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         private const string lockKey = "DistancifyMediaMapperRunner";
 
         public Runner(
             IIoCContainer container,
             EventBroker eventBroker,
+            IApplicationLifetime applicationLifetime,
             DistributedLockService distributedLockService) : this(
             container,
             eventBroker,
+            applicationLifetime,
             distributedLockService,
             ConfigurationManager.AppSettings["MediaMapperEnabled"])
         {
@@ -36,6 +42,7 @@ namespace Distancify.LitiumAddOns.MediaMapper
         public Runner(
             IIoCContainer container,
             EventBroker eventBroker,
+            IApplicationLifetime applicationLifetime,
             DistributedLockService distributedLockService,
             string enabledSetting)
         {
@@ -56,6 +63,44 @@ namespace Distancify.LitiumAddOns.MediaMapper
                     }
                 }
             });
+
+            applicationLifetime.ApplicationStarted.Register(() =>
+            {
+                _task = Task.Run(() =>
+                {
+                    using (Solution.Instance.SystemToken.Use())
+                    {
+                        while (!_cancellationTokenSource.Token.IsCancellationRequested)
+                        {
+                            if (SleepUnlessCancelled(5, _cancellationTokenSource.Token)) return;
+
+                            using (distributedLockService.AcquireLock(lockKey, TimeSpan.FromSeconds(10)))
+                            {
+                                foreach (var m in container.ResolveAll<IMediaMapper>())
+                                {
+                                    m.Map();
+                                }
+                            }
+                        }
+                    }
+                }, _cancellationTokenSource.Token);
+            });
+
+            applicationLifetime.ApplicationStopping.Register(() =>
+            {
+                _cancellationTokenSource.Cancel();
+                subscription.Dispose();
+            });
+        }
+
+        private bool SleepUnlessCancelled(int minutes, CancellationToken token)
+        {
+            for (int i = 0; i < minutes * 60; i++)
+            {
+                if (_cancellationTokenSource.Token.IsCancellationRequested) return _cancellationTokenSource.Token.IsCancellationRequested;
+                Thread.Sleep(1000);
+            }
+            return _cancellationTokenSource.Token.IsCancellationRequested;
         }
     }
 }
